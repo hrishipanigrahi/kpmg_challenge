9dfaf2d5d2f227642e53dfef22784d774e801b0f
Hrishikesh Panigrahi <30368891+hrishipanigrahi@users.noreply.github.com>
2023-02-08 21:39:57 +0530

    Create challenge3.py

---
+++ challenge3/challenge3.py
@@ -0,0 +1,24 @@
+def getKey(obj: dict):
+    keys = list(obj)
+    if len(keys) != 1:
+        raise Exception('either multiple keys or empty dict found')
+    else:
+        return keys[0]
+
+def getNestedValue(obj: dict, key: str):
+    if type(obj) is not dict :
+        return None
+    if (key in obj.keys()) :
+        if type(obj[key]) is dict:
+            return getNestedValue(obj[key], getKey(obj[key]))
+        else:
+            # print(f'obj[getKey(obj)]: {obj[getKey(obj)]}')
+            return obj[getKey(obj)]
+    else:
+        nestedKey = getKey(obj)
+        return getNestedValue(obj[nestedKey], key)
+
+if __name__ == '__main__':
+    obj = {'a': {'b': {'c': 'd'}}}
+    value = getNestedValue(obj, 'c')
+    print(value)
